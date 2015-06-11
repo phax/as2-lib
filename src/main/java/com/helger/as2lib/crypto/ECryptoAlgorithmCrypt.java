12a70ebfb0637081229778815309979b8a56f2d4
phax <philip@helger.com>
2015-06-11 10:54:32 +0200

    Split ECryptoAlgorithm into 2 separate enums

---
+++ src/main/java/com/helger/as2lib/crypto/ECryptoAlgorithmCrypt.java
@@ -0,0 +1,79 @@
+/**
+ * The FreeBSD Copyright
+ * Copyright 1994-2008 The FreeBSD Project. All rights reserved.
+ * Copyright (C) 2013-2015 Philip Helger philip[at]helger[dot]com
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE FREEBSD PROJECT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * official policies, either expressed or implied, of the FreeBSD Project.
+ */
+package com.helger.as2lib.crypto;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.cms.CMSAlgorithm;
+
+import com.helger.commons.annotations.Nonempty;
+import com.helger.commons.lang.EnumHelper;
+
+public enum ECryptoAlgorithmCrypt implements ICryptoAlgorithm
+{
+  CRYPT_3DES ("3des", PKCSObjectIdentifiers.des_EDE3_CBC),
+  CRYPT_CAST5 ("cast5", CMSAlgorithm.CAST5_CBC),
+  CRYPT_IDEA ("idea", CMSAlgorithm.IDEA_CBC),
+  CRYPT_RC2 ("rc2", PKCSObjectIdentifiers.RC2_CBC);
+
+  private final String m_sID;
+  private final ASN1ObjectIdentifier m_aOID;
+
+  private ECryptoAlgorithmCrypt (@Nonnull @Nonempty final String sID, @Nonnull final ASN1ObjectIdentifier aOID)
+  {
+    m_sID = sID;
+    m_aOID = aOID;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
+  }
+
+  @Nonnull
+  public ASN1ObjectIdentifier getOID ()
+  {
+    return m_aOID;
+  }
+
+  @Nullable
+  public static ECryptoAlgorithmCrypt getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (ECryptoAlgorithmCrypt.class, sID);
+  }
+}
---
+++ src/main/java/com/helger/as2lib/crypto/ECryptoAlgorithmSign.java
@@ -0,0 +1,88 @@
+/**
+ * The FreeBSD Copyright
+ * Copyright 1994-2008 The FreeBSD Project. All rights reserved.
+ * Copyright (C) 2013-2015 Philip Helger philip[at]helger[dot]com
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE FREEBSD PROJECT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * official policies, either expressed or implied, of the FreeBSD Project.
+ */
+package com.helger.as2lib.crypto;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+
+import com.helger.commons.annotations.Nonempty;
+import com.helger.commons.lang.EnumHelper;
+
+/**
+ * This enum contains all signing supported crypto algorithms. The algorithms
+ * contained in here may not be used for encryption of anything. See
+ * {@link ECryptoAlgorithmCrypt} for encryption algorithms.
+ *
+ * @author Philip Helger
+ */
+public enum ECryptoAlgorithmSign implements ICryptoAlgorithm
+{
+  DIGEST_MD5 ("md5", PKCSObjectIdentifiers.md5),
+  DIGEST_SHA1 ("sha1", OIWObjectIdentifiers.idSHA1),
+  DIGEST_SHA256 ("sha256", NISTObjectIdentifiers.id_sha256),
+  DIGEST_SHA384 ("sha384", NISTObjectIdentifiers.id_sha384),
+  DIGEST_SHA512 ("sha512", NISTObjectIdentifiers.id_sha512);
+
+  private final String m_sID;
+  private final ASN1ObjectIdentifier m_aOID;
+
+  private ECryptoAlgorithmSign (@Nonnull @Nonempty final String sID, @Nonnull final ASN1ObjectIdentifier aOID)
+  {
+    m_sID = sID;
+    m_aOID = aOID;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
+  }
+
+  @Nonnull
+  public ASN1ObjectIdentifier getOID ()
+  {
+    return m_aOID;
+  }
+
+  @Nullable
+  public static ECryptoAlgorithmSign getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (ECryptoAlgorithmSign.class, sID);
+  }
+}
