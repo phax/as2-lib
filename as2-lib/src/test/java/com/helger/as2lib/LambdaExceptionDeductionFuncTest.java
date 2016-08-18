0eb9fa36b9abe8704bd26a7692aa56a0d17f1742
Philip Helger <philip@helger.com>
2016-08-18 13:26:37 +0200

    Added test case

---
+++ as2-lib/src/test/java/com/helger/as2lib/LambdaExceptionDeductionFuncTest.java
@@ -0,0 +1,36 @@
+package com.helger.as2lib;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * This is a dummy class to test for generic exceptions in combinations with
+ * Lambdas
+ *
+ * @author Philip Helger
+ */
+public class LambdaExceptionDeductionFuncTest
+{
+  @FunctionalInterface
+  public static interface IThrowingGetter <EX extends Exception>
+  {
+    String get () throws EX;
+  }
+
+  @FunctionalInterface
+  public static interface ICreator <EX extends Exception>
+  {
+    IThrowingGetter <EX> create ();
+  }
+
+  public static void main (final String [] args) throws IOException
+  {
+    final InputStream aIS = new ByteArrayInputStream ("abc".getBytes (StandardCharsets.UTF_8));
+    final IThrowingGetter <IOException> aGetter = () -> Character.toString ((char) aIS.read ());
+    final ICreator <IOException> aCreator = () -> aGetter;
+    // Expected outcome: ab
+    System.out.println (aCreator.create ().get () + aCreator.create ().get ());
+  }
+}
