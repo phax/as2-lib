234d52b60218d6d274a59614694877694b0fab17
Philip Helger <philip@helger.com>
2018-05-02 22:48:03 +0200

    Simple web app listener for #36

---
+++ as2-servlet/src/main/java/com/helger/as2servlet/AS2WebAppListener.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015-2018 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.as2servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.helger.web.scope.mgr.WebScopeManager;
+
+/**
+ * A very simple listener, that setups the framework for running the AS2
+ * servlet. Use this as a &lt;listener&gt; in your <code>web.xml</code>.
+ *
+ * @author Philip Helger
+ */
+public class AS2WebAppListener implements ServletContextListener
+{
+  public void contextInitialized (final ServletContextEvent aSCE)
+  {
+    final ServletContext aSC = aSCE.getServletContext ();
+    WebScopeManager.onGlobalBegin (aSC);
+  }
+
+  public void contextDestroyed (final ServletContextEvent aSce)
+  {
+    WebScopeManager.onGlobalEnd ();
+  }
+}
