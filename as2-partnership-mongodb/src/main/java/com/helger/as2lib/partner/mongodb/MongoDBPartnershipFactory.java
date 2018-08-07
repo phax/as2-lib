234ceae9e13e338ab990bd3be6a503aa665aaac1
Philip Helger <philip@helger.com>
2018-08-07 09:29:03 +0200

    Fixed naming

--- as2-partnership-mongodb/src/main/java/com/helger/as2lib/partner/mongodb/MongoDBPartnershipFactory.java
+++ as2-partnership-mongodb/src/main/java/com/helger/as2lib/partner/mongodb/MongoDBPartnershipFactory.java
@@ -48,104 +48,123 @@ import com.mongodb.client.result.DeleteResult;
  * @author jochenberger
  */
 @CodingStyleguideUnaware
-public class MongoDBPartnershipFactory extends AbstractDynamicComponent implements IPartnershipFactory {
+public class MongoDBPartnershipFactory extends AbstractDynamicComponent implements IPartnershipFactory
+{
   private static final long serialVersionUID = -2282798646250446937L;
   private static final String NAME_KEY = "name";
   private static final String RECEIVER_IDS = "receiver-ids";
   private static final String SENDER_IDS = "sender-ids";
   private static final String ATTRIBUTES = "attributes";
 
-  private final MongoCollection <Document> partnerships;
-  private final Logger logger;
+  private final MongoCollection <Document> m_aPartnerships;
+  private final Logger m_aLogger;
 
-  public MongoDBPartnershipFactory (final MongoCollection <Document> partnerships, final Logger logger) {
-    this.logger = logger;
-    partnerships.createIndex (new Document (NAME_KEY, Integer.valueOf (1)), new IndexOptions ().unique (true));
-    this.partnerships = partnerships;
+  public MongoDBPartnershipFactory (final MongoCollection <Document> aPartnerships, final Logger aLogger)
+  {
+    m_aLogger = aLogger;
+    aPartnerships.createIndex (new Document (NAME_KEY, Integer.valueOf (1)), new IndexOptions ().unique (true));
+    m_aPartnerships = aPartnerships;
   }
 
   @Override
-  public EChange addPartnership (final Partnership aPartnership) throws OpenAS2Exception {
-    partnerships.insertOne (toDocument (aPartnership));
+  public EChange addPartnership (final Partnership aPartnership) throws OpenAS2Exception
+  {
+    m_aPartnerships.insertOne (_toDocument (aPartnership));
     return EChange.CHANGED;
   }
 
   @Override
-  public EChange removePartnership (final Partnership aPartnership) throws OpenAS2Exception {
-    final DeleteResult result = partnerships.deleteOne (new Document (NAME_KEY, aPartnership.getName ()));
-    if (result.getDeletedCount () >= 1l) {
+  public EChange removePartnership (final Partnership aPartnership) throws OpenAS2Exception
+  {
+    final DeleteResult result = m_aPartnerships.deleteOne (new Document (NAME_KEY, aPartnership.getName ()));
+    if (result.getDeletedCount () >= 1l)
+    {
       return EChange.CHANGED;
     }
     return EChange.UNCHANGED;
   }
 
   @Override
-  public void updatePartnership (final IMessage aMsg, final boolean bOverwrite) throws OpenAS2Exception {
+  public void updatePartnership (final IMessage aMsg, final boolean bOverwrite) throws OpenAS2Exception
+  {
     // Fill in any available partnership information
     final Partnership aPartnership = getPartnership (aMsg.partnership ());
 
-    logger.debug ("Updating partnership {}", aPartnership);
+    m_aLogger.debug ("Updating partnership {}", aPartnership);
 
     // Update partnership data of message with the stored ones
     aMsg.partnership ().copyFrom (aPartnership);
 
     // Set attributes
-    if (bOverwrite) {
+    if (bOverwrite)
+    {
       final String sSubject = aPartnership.getAttribute (CPartnershipIDs.PA_SUBJECT);
-      if (sSubject != null) {
+      if (sSubject != null)
+      {
         aMsg.setSubject (new MessageParameters (aMsg).format (sSubject));
       }
     }
   }
 
   @Override
-  public void updatePartnership (final IMessageMDN aMdn, final boolean bOverwrite) throws OpenAS2Exception {
+  public void updatePartnership (final IMessageMDN aMdn, final boolean bOverwrite) throws OpenAS2Exception
+  {
     final Partnership aPartnership = getPartnership (aMdn.partnership ());
     aMdn.partnership ().copyFrom (aPartnership);
   }
 
   @Override
-  public Partnership getPartnership (final Partnership aPartnership) throws OpenAS2Exception {
+  public Partnership getPartnership (final Partnership aPartnership) throws OpenAS2Exception
+  {
     Partnership aRealPartnership = getPartnershipByName (aPartnership.getName ());
-    if (aRealPartnership == null) {
+    if (aRealPartnership == null)
+    {
       // Found no partnership by name
       aRealPartnership = getPartnershipByID (aPartnership.getAllSenderIDs (), aPartnership.getAllReceiverIDs ());
     }
 
-    if (aRealPartnership == null) {
+    if (aRealPartnership == null)
+    {
       throw new PartnershipNotFoundException (aPartnership);
     }
     return aRealPartnership;
   }
 
-  private Partnership getPartnershipByID (final IStringMap allSenderIDs, final IStringMap allReceiverIDs) {
+  private Partnership getPartnershipByID (final IStringMap allSenderIDs, final IStringMap allReceiverIDs)
+  {
     Document filter = new Document ();
-    for (final Map.Entry <String, String> entry : allSenderIDs.entrySet ()) {
+    for (final Map.Entry <String, String> entry : allSenderIDs.entrySet ())
+    {
       filter.append (SENDER_IDS + "." + entry.getKey (), entry.getValue ());
     }
-    for (final Map.Entry <String, String> entry : allReceiverIDs.entrySet ()) {
+    for (final Map.Entry <String, String> entry : allReceiverIDs.entrySet ())
+    {
       filter.append (RECEIVER_IDS + "." + entry.getKey (), entry.getValue ());
     }
 
-    Partnership result = partnerships.find (filter).map (MongoDBPartnershipFactory::toPartnership).first ();
-    if (result != null) {
+    Partnership result = m_aPartnerships.find (filter).map (MongoDBPartnershipFactory::_toPartnership).first ();
+    if (result != null)
+    {
       return result;
     }
 
     // try the other way around, maybe we're receiving a response
     // TODO is this really a good idea?
     filter = new Document ();
-    for (final Map.Entry <String, String> entry : allSenderIDs.entrySet ()) {
+    for (final Map.Entry <String, String> entry : allSenderIDs.entrySet ())
+    {
       filter.append (RECEIVER_IDS + "." + entry.getKey (), entry.getValue ());
     }
-    for (final Map.Entry <String, String> entry : allReceiverIDs.entrySet ()) {
+    for (final Map.Entry <String, String> entry : allReceiverIDs.entrySet ())
+    {
       filter.append (SENDER_IDS + "." + entry.getKey (), entry.getValue ());
     }
 
-    final Partnership inverseResult = partnerships.find (filter)
-                                                  .map (MongoDBPartnershipFactory::toPartnership)
-                                                  .first ();
-    if (inverseResult != null) {
+    final Partnership inverseResult = m_aPartnerships.find (filter)
+                                                     .map (MongoDBPartnershipFactory::_toPartnership)
+                                                     .first ();
+    if (inverseResult != null)
+    {
       result = new Partnership (inverseResult.getName () + "-inverse");
       result.setReceiverX509Alias (inverseResult.getSenderX509Alias ());
       result.setReceiverAS2ID (inverseResult.getSenderAS2ID ());
@@ -159,58 +178,71 @@ public class MongoDBPartnershipFactory extends AbstractDynamicComponent implemen
   }
 
   @Override
-  public Partnership getPartnershipByName (final String sName) {
-    return partnerships.find (new Document (NAME_KEY, sName)).map (MongoDBPartnershipFactory::toPartnership).first ();
+  public Partnership getPartnershipByName (final String sName)
+  {
+    return m_aPartnerships.find (new Document (NAME_KEY, sName))
+                          .map (MongoDBPartnershipFactory::_toPartnership)
+                          .first ();
   }
 
   @Override
-  public ICommonsSet <String> getAllPartnershipNames () {
-    return partnerships.distinct (NAME_KEY, String.class).into (new CommonsHashSet <> ());
+  public ICommonsSet <String> getAllPartnershipNames ()
+  {
+    return m_aPartnerships.distinct (NAME_KEY, String.class).into (new CommonsHashSet <> ());
   }
 
   @Override
-  public ICommonsList <Partnership> getAllPartnerships () {
-    return partnerships.find ().map (MongoDBPartnershipFactory::toPartnership).into (new CommonsArrayList <> ());
+  public ICommonsList <Partnership> getAllPartnerships ()
+  {
+    return m_aPartnerships.find ().map (MongoDBPartnershipFactory::_toPartnership).into (new CommonsArrayList <> ());
   }
 
-  private static Document toDocument (final IStringMap stringMap) {
+  private static Document _toDocument (final IStringMap stringMap)
+  {
     final Document document = new Document ();
-    for (final Map.Entry <String, String> entry : stringMap.entrySet ()) {
+    for (final Map.Entry <String, String> entry : stringMap.entrySet ())
+    {
       document.put (entry.getKey (), entry.getValue ());
     }
     return document;
   }
 
-  private static Document toDocument (final Partnership partnership) {
+  private static Document _toDocument (final Partnership partnership)
+  {
     final Document document = new Document ();
     document.put (NAME_KEY, partnership.getName ());
-    document.put (RECEIVER_IDS, toDocument (partnership.getAllReceiverIDs ()));
-    document.put (SENDER_IDS, toDocument (partnership.getAllSenderIDs ()));
-    document.put (ATTRIBUTES, toDocument (partnership.getAllAttributes ()));
+    document.put (RECEIVER_IDS, _toDocument (partnership.getAllReceiverIDs ()));
+    document.put (SENDER_IDS, _toDocument (partnership.getAllSenderIDs ()));
+    document.put (ATTRIBUTES, _toDocument (partnership.getAllAttributes ()));
 
     return document;
   }
 
-  private static Partnership toPartnership (final Document document) {
+  private static Partnership _toPartnership (final Document document)
+  {
     final Partnership partnership = new Partnership (document.getString (NAME_KEY));
     final Document senderIDs = (Document) document.get (SENDER_IDS);
     final ICommonsMap <String, String> senders = new CommonsHashMap <> (senderIDs.size ());
-    for (final Map.Entry <String, Object> e : senderIDs.entrySet ()) {
+    for (final Map.Entry <String, Object> e : senderIDs.entrySet ())
+    {
       senders.put (e.getKey (), e.getValue ().toString ());
     }
     partnership.addSenderIDs (senders);
 
     final Document receiverIDs = (Document) document.get (RECEIVER_IDS);
     final ICommonsMap <String, String> receivers = new CommonsHashMap <> (receiverIDs.size ());
-    for (final Map.Entry <String, Object> e : receiverIDs.entrySet ()) {
+    for (final Map.Entry <String, Object> e : receiverIDs.entrySet ())
+    {
       receivers.put (e.getKey (), e.getValue ().toString ());
     }
     partnership.addReceiverIDs (receivers);
 
     final Document attributes = (Document) document.get (ATTRIBUTES);
-    if (attributes != null) {
+    if (attributes != null)
+    {
       final ICommonsMap <String, String> att = new CommonsHashMap <> (receiverIDs.size ());
-      for (final Map.Entry <String, Object> e : attributes.entrySet ()) {
+      for (final Map.Entry <String, Object> e : attributes.entrySet ())
+      {
         att.put (e.getKey (), e.getValue ().toString ());
       }
       partnership.addAllAttributes (att);
