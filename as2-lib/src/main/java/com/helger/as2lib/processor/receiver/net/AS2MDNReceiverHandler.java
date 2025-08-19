1ea2dfcd997b6954219c4fb2a8db5a4a1d6f6a6d
Philip Helger <philip@helger.com>
2025-08-19 09:58:22 +0200

    Fixed certificate selection for async MDNs

--- as2-lib/src/main/java/com/helger/as2lib/processor/receiver/net/AS2MDNReceiverHandler.java
+++ as2-lib/src/main/java/com/helger/as2lib/processor/receiver/net/AS2MDNReceiverHandler.java
@@ -110,8 +110,7 @@ public class AS2MDNReceiverHandler extends AbstractReceiverHandler
 
   /**
    * @param aModule
-   *        The receiver module for attributes, session etc. May not be
-   *        <code>null</code>.
+   *        The receiver module for attributes, session etc. May not be <code>null</code>.
    */
   public AS2MDNReceiverHandler (@Nonnull final AS2MDNReceiverModule aModule)
   {
@@ -119,8 +118,7 @@ public class AS2MDNReceiverHandler extends AbstractReceiverHandler
   }
 
   /**
-   * @return The receiver module passed in the constructor. Never
-   *         <code>null</code>.
+   * @return The receiver module passed in the constructor. Never <code>null</code>.
    */
   @Nonnull
   public final AS2MDNReceiverModule getModule ()
@@ -294,8 +292,7 @@ public class AS2MDNReceiverHandler extends AbstractReceiverHandler
    *
    * @param aMsg
    *        Message
-   * @return <code>true</code> if the MDN was processed, <code>false</code> e.g.
-   *         on MIC mismatch
+   * @return <code>true</code> if the MDN was processed, <code>false</code> e.g. on MIC mismatch
    * @throws AS2Exception
    *         In case of error; e.g. MIC mismatch
    */
@@ -399,8 +396,8 @@ public class AS2MDNReceiverHandler extends AbstractReceiverHandler
       aMDN.partnership ().setSenderAS2ID (aMDN.getHeader (CHttpHeader.AS2_FROM));
       aMDN.partnership ().setReceiverAS2ID (aMDN.getHeader (CHttpHeader.AS2_TO));
       // Set the appropriate keystore aliases
-      aMDN.partnership ().setSenderX509Alias (aMsg.partnership ().getReceiverX509Alias ());
-      aMDN.partnership ().setReceiverX509Alias (aMsg.partnership ().getSenderX509Alias ());
+      aMDN.partnership ().setSenderX509Alias (aMsg.partnership ().getSenderX509Alias ());
+      aMDN.partnership ().setReceiverX509Alias (aMsg.partnership ().getReceiverX509Alias ());
       // Update the partnership
       getModule ().getSession ().getPartnershipFactory ().updatePartnership (aMDN, false);
 
